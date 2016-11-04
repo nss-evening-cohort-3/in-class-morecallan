c61629271ee948447757b9ee59d1f77a55bd615b
Callan Morrison <callan.l.morrison@gmail.com>
2016-11-03 21:42:52 -0500

    Initial Setup

--- BasicsOfForms/App_Start/BundleConfig.cs
+++ BasicsOfForms/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace BasicsOfForms
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.min.js",
+                      "~/Scripts/app.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
