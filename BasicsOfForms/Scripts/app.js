5c114649316d85a0651f32c8ff54847310766ceb
Callan Morrison <callan.l.morrison@gmail.com>
2016-11-03 23:13:30 -0500

    Create an Angular Driven Form

--- BasicsOfForms/Scripts/app.js
+++ BasicsOfForms/Scripts/app.js
@@ -4,6 +4,7 @@ var app = angular.module("MainApp", []);
 
 app.controller('FormController', function ($scope) {
     $scope.formSubmit = function () {
-
+        var serializedForm = $("#myForm").serialize()
+        console.log(serializedForm)
     }
 });
